c06f025e417003e75145de8415de62920b93e503
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-06-10 21:03:16 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 // require('dotenv').config(); // works
-import dotenv from 'dotenv';
-dotenv.config();
 // import 'dotenv/config'; //works 
+import dotenv from 'dotenv'; // works
+dotenv.config();
 
 import connectDB from './db/index.js';
+import {app} from './app.js';
 
-connectDB();
+
+connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log('ERROR: ', error);
+        process.exit(1);
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log('MONGODB connection failed !!!', error);
+})
 
 
 
