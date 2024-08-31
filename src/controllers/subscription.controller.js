bfb89a213c1fd2a2394ab00a497d81cefd2bee1e
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-08-31 16:08:06 +0530

    create controller for getting list of all subscribers of a channel

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from '../utils/ApiError.js';
 import {User} from '../models/user.model.js';
 import {Subscription} from '../models/subscription.model.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
+import mongoose from 'mongoose';
 
 
 
@@ -36,3 +37,78 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 
 
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params;
+    if(!channelId){
+        throw new ApiError(404, "Channel not found!");
+    }
+  
+    const channelExists = await User.exists({_id: channelId});
+    if(!channelExists){
+        throw new ApiError(404, "Channel not found!");
+    }
+    
+    const subscribersList = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: 'subscriptions',
+                localField: '_id',
+                foreignField: 'channel',
+                as: 'subscribers',
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: 'users',
+                            localField: '_id',
+                            foreignField: 'channel',
+                            as: 'subscriber',
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }  
+                    },
+                    {
+                        $addFields: {
+                            subscriber: {
+                               $first: "$subscriber" 
+                            }
+                        }
+                    },
+                    {
+                        $project: {
+                            suscriber: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                subscribers: {
+                   $first: '$subscriptions' 
+                }
+            }
+        },
+        {
+            $project: {
+                subscribers: 1
+            }
+        }
+    ]);
+});
+
+
+
+const getUserSubscribedChannels = asyncHandler(async (req, res) => {
+});
