253ec0cdb123e0189705bc50cad735cce7d1ca8e
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-03 19:35:27 +0530

    setup endpoints for all playlist controlllers

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,22 @@
+import {Router} from 'express';
+import {verifyJWT} from '../middlewares/auth.middleware.js';
+import {getPlaylistById, createPlaylist, addVideoToPlaylist, removeVideoFromPlaylist, updatePlaylist, deletePlaylist, getUserChannelPlaylists} from '../controllers/playlist.controller.js';
+
+
+const router = Router();
+
+router.route('/').post(verifyJWT, createPlaylist);
+
+router.route('/:playlistId')
+.get(getPlaylistById)
+.patch(verifyJWT, updatePlaylist)
+.delete(verifyJWT, deletePlaylist);
+
+router.route('/:playlistId/v/:videoId')
+.patch(verifyJWT, addVideoToPlaylist)
+.patch(verifyJWT, removeVideoFromPlaylist);
+
+router.route('/user/:userId').get(getUserChannelPlaylists);
+
+
+export default router;
