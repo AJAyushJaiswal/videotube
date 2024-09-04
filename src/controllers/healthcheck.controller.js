3a5677e2e5fb862e80576519db6de3ed62638ef9
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-04 15:30:40 +0530

    test healthcheck controller using postman

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,7 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {ApiError} from '../utils/ApiError.js';
 
-
+// tested using postman -- working properly
 const healthCheck = asyncHandler(async (req, res) => {
     try{
         res.status(200).json(new ApiResponse(200, null, "Health check successfull!"));
